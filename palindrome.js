28a8c68cba9d9100114a8b9a0a071e3ccd34a240
ak1code <akshay.ramoliya9@gmail.com>
2023-02-12 10:40:00 +0530

    palindrom

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+let str = "nayan";
+let bag = "";
+
+for (i = str.length - 1; i >= 0; i--) {
+  bag += str[i];
+}
+if (bag !== str)
+  console.log("palindrome")
+else {
+  console.log("Not palindrome")
+}
